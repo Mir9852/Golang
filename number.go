33ce0270594a47e53d848f15056004f3cd2beb1a
Muhamad Amir <muh52amir@gmail.com>
2025-08-27 20:30:27 +0700

    upload ke3

--- number.go
+++ number.go
@@ -1,9 +1,9 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	fmt.Println("satu = ", 1)
-	fmt.Println("dua = ", 2)
-	fmt.Println("tiga koma lima = ", 3.5)
-}
+// func main() {
+// 	fmt.Println("satu = ", 1)
+// 	fmt.Println("dua = ", 2)
+// 	fmt.Println("tiga koma lima = ", 3.5)
+// }
---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Muhamad")
+	fmt.Println("Amir")
+	fmt.Println("Muhammad Amir")
+
+	fmt.Println(len("Muhamad"))
+	fmt.Println("Amir"[0])
+	fmt.Println("Muhammad Amir"[1])
+}
